05c037efb2cb1e6387e49f10671b6397914544e1
drazzf <162735044+MihailovTommy@users.noreply.github.com>
2024-04-16 17:07:32 +0300

    Add files via upload

--- script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("text-container").addEventListener("click", function() {
-    var link = document.getElementById("invisible-link");
-    if (link) {
-        link.removeAttribute("hidden"); // Делаем скрытую ссылку видимой
-        link.click(); // Эмулируем клик по ссылке
-        link.setAttribute("hidden", ""); // Скрываем ссылку снова после клика
-    }
-});
